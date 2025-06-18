3ee4ae5506a1b2c48ddd8caaf9e6c9ef39aa4a62
woogy9999 <gusdnrdkshf@gmail.com>
2025-06-18 10:13:29 +0900

    devops

---
+++ src/test/java/com/sist/web/Devops4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sist.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devops4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
